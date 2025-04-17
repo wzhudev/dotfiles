05ae6146c310aa99fe92b5f4e00f8ac99ea18edf
Wenzhao Hu <12122021+wzhudev@users.noreply.github.com>
2025-04-17 10:58:19 +0800

    update vim and disable animation

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -4,19 +4,19 @@ return {
     return vim.tbl_deep_extend("force", opts, {
       scroll = { enabled = false },
       explorer = {
-        enabled = true, -- 启用文件浏览器
-        hidden = true, -- 显示隐藏文件（例如 .gitignore）
-        ignored = true, -- 显示被 Git 忽略的文件
+        enabled = true,
+        hidden = true,
+        ignored = true,
       },
       picker = {
         sources = {
           explorer = {
-            hidden = true, -- 在 picker 的 explorer 中显示隐藏文件
-            ignored = true, -- 在 picker 的 explorer 中显示被 Git 忽略的文件
+            hidden = true,
+            ignored = true,
           },
           files = {
-            hidden = true, -- 在 picker 的 explorer 中显示隐藏文件
-            ignored = true, -- 在 picker 的 explorer 中显示被 Git 忽略的文件
+            hidden = true,
+            ignored = true,
             exclude = { "**/node_modules/*" },
           },
         },
