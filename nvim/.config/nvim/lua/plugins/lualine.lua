acb194e4f15bb64ec7570d39ffabf0ca3b01d50a
Wenzhao Hu <12122021+wzhudev@users.noreply.github.com>
2025-06-08 19:19:50 +0800

    Revert "feat: use good tools"
    This reverts commit 217ce9e0f8e91199c15927c20b1146ddf79106ef.

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      local icons = LazyVim.config.icons
+      opts.sections.lualine_c = {
+        LazyVim.lualine.root_dir(),
+        {
+          "diagnostics",
+          symbols = {
+            error = icons.diagnostics.Error,
+            warn = icons.diagnostics.Warn,
+            info = icons.diagnostics.Info,
+            hint = icons.diagnostics.Hint,
+          },
+        },
+        { "filename", path = 1 }, -- show full path
+      }
+    end,
+  },
+}
